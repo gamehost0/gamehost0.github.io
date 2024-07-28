32d6ecdbea2c724bd6435034540aa5349610df2c
ShanBee <148629373+ShanBee@users.noreply.github.com>
2024-07-28 14:01:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ const scoreDisplay = document.getElementById("score");
 // Lisää klikkaustapahtuma kuvaan
 popdogImg.addEventListener("click", () => {
     score++; // Lisää pisteitä klikkauksesta
-    scoreDisplay.textContent = "Pisteet: " + score; // Päivitä pistemäärä
+    scoreDisplay.textContent = "Pisteet: {_score++}" + score; // Päivitä pistemäärä
+    alert("PoPDoG ei näytä tällä hetkellä pisteitä pisteet kohdassa. Voit ottaa tämän viestin pois käytöstä, klikkaamalla Älä anna tämän sivuston näyttää tätä uudelleen")
 });
