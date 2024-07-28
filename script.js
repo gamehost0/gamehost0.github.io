0113ef448e66340af5aecb0cfc8acd23e9a2025c
ShanBee <148629373+ShanBee@users.noreply.github.com>
2024-07-28 14:03:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let score = 0;
+
 // Hae elementit
 const popdogImg = document.getElementById("popdog-img");
 const scoreDisplay = document.getElementById("score");
@@ -5,6 +7,6 @@ const scoreDisplay = document.getElementById("score");
 // Lisää klikkaustapahtuma kuvaan
 popdogImg.addEventListener("click", () => {
     score++; // Lisää pisteitä klikkauksesta
-    scoreDisplay.textContent = "Pisteet: {_score++}" + score; // Päivitä pistemäärä
-    alert("PoPDoG ei näytä tällä hetkellä pisteitä pisteet kohdassa. Voit ottaa tämän viestin pois käytöstä, klikkaamalla Älä anna tämän sivuston näyttää tätä uudelleen")
+    scoreDisplay.textContent = "Pisteet: " + score; // Päivitä pistemäärä
+});
 });
